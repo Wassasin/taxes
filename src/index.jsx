10b15bc44fc6425f4b867d7c93bc8e9850ebf08c
Wouter Geraedts <wouter@tweedegolf.com>
2017-12-27 12:14:58 +0100

    Added baseline graph

--- src/index.jsx
+++ src/index.jsx
@@ -1,59 +1,45 @@
 import './index.scss';
+import Chart from 'chart.js';
 
-class Particle {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-
-  update() {
-    this.x = (this.x + 0.01) % 1.0;
-  }
-
-  render(ctx) {
-      const width = 1.0/300.0;
-      const height = 1.0/300.0;
-
-      ctx.fillStyle = 'white';
-      ctx.fillRect(this.x - width / 2.0, this.y - height / 2.0, width, height);
-  }
-}
-
-class ParticleEngine {
-  constructor() {
-    this.particles = [];
-
-    for(let i = 0; i < 100; i += 1) {
-      this.particles.push(new Particle(Math.random(), Math.random()));
-    }
-  }
-
-  update() {
-    this.particles.forEach(p => p.update());
-  }
-
-  render(ctx) {
-    ctx.save();
-    ctx.canvas.width = window.innerWidth;
-    ctx.canvas.height = window.innerHeight;
-    ctx.scale(ctx.canvas.width, ctx.canvas.height);
-    // ctx.clearRect(0.0, 0.0, 1.0, 1.0);
-    this.particles.forEach(p => p.render(ctx));
-    ctx.restore();
-  }
-}
+import _map from 'lodash/map';
+import _range from 'lodash/range';
 
 const root = document.getElementById("root");
 const canvas = document.createElement("canvas");
 root.appendChild(canvas);
 
-const ctx = canvas.getContext('2d');
-const pe = new ParticleEngine();
-
-const draw_f = () => {
-  pe.update();
-  pe.render(ctx)
-  window.requestAnimationFrame(draw_f);
+const xAxis = {
+  min: 10000,
+  max: 50000,
+  steps: 500,
 };
 
-window.requestAnimationFrame(draw_f);
+const data = _map(_range(xAxis.min, xAxis.max, xAxis.steps), hv => {
+  return {
+    x: hv,
+    y: hv
+  };
+});
+
+const ctx = canvas.getContext('2d');
+const chart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    datasets: [ {
+      label: 'Netto bestedingsvermogen',
+      data: data,
+    } ]
+  },
+  options: {
+    maintainAspectRatio: false,
+    scales: {
+      xAxes: [ {
+        type: 'linear',
+        ticks: {
+          min: xAxis.min,
+          max: xAxis.max,
+        }
+      } ]
+    }
+  }
+});
