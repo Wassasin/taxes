d39ee7bf11fcc1a1978a294336ef0d9d092faae5
Wouter Geraedts <wouter@tweedegolf.com>
2017-12-27 14:46:03 +0100

    Finalized datasets

--- src/index.jsx
+++ src/index.jsx
@@ -9,9 +9,9 @@ const canvas = document.createElement("canvas");
 root.appendChild(canvas);
 
 const xAxis = {
-  min: 10000,
-  max: 50000,
-  steps: 200,
+  min: 0,
+  max: 60000,
+  steps: 250,
 };
 
 // Alles voor 2017
@@ -61,6 +61,59 @@ const computeZorgtoeslag = (hi) => {
   return Math.max(standaardpremie - normpremie, 0);
 };
 
+const computeInkomstenbelasting = (hi) => {
+  const eersteSchijf = 19982;
+  const tweedeSchijf = 33791;
+  const derdeSchijf = 67072;
+
+  // https://www.nibud.nl/consumenten/inkomstenbelasting/
+
+  let inkomstenbelasting = 0;
+
+  inkomstenbelasting += 0.3655 * Math.min(hi, eersteSchijf);
+  inkomstenbelasting += 0.4080 * Math.max(Math.min(hi, tweedeSchijf) - eersteSchijf, 0);
+  inkomstenbelasting += 0.4080 * Math.max(Math.min(hi, derdeSchijf) - tweedeSchijf, 0);
+  inkomstenbelasting += 0.5200 * Math.max(hi - derdeSchijf, 0);
+
+  return inkomstenbelasting;
+};
+
+const computeHeffingskorting = (hi) => {
+  // https://www.belastingdienst.nl/wps/wcm/connect/bldcontentnl/belastingdienst/prive/inkomstenbelasting/heffingskortingen_boxen_tarieven/heffingskortingen/algemene_heffingskorting/tabel-ahk-2017
+
+  if (hi < 19982) {
+    return 2254;
+  }
+
+  if (hi > 67068) {
+    return 0;
+  }
+
+  return Math.max(2254 - 0.04787 * (hi - 19982), 0);
+};
+
+const computeArbeidskorting = (hi) => {
+  // https://www.belastingdienst.nl/wps/wcm/connect/bldcontentnl/belastingdienst/prive/inkomstenbelasting/heffingskortingen_boxen_tarieven/heffingskortingen/arbeidskorting/tabel_arbeidskorting_2017
+
+  if (hi < 9309) {
+    return hi * 0.01772;
+  }
+
+  if (hi < 20108) {
+    return 165 + 0.28317 * (hi - 9309);
+  }
+
+  if (hi < 32444) {
+    return 3223;
+  }
+
+  if (hi < 121972) {
+    return 3223 - 0.036 * (hi - 32444);
+  }
+
+  return 0;
+};
+
 const generateSeries = f => _map(_range(xAxis.min, xAxis.max, xAxis.steps), x => {
   return {
     x: x,
@@ -73,45 +126,64 @@ const nettoData = generateSeries(hi => {
 
   ni += computeHuurtoeslag(hi, 700);
   ni += computeZorgtoeslag(hi);
+  ni += computeHeffingskorting(hi);
+  ni += computeArbeidskorting(hi);
+  ni -= computeInkomstenbelasting(hi);
 
   return ni;
 });
 
 const invisible = 'rgba(0, 0, 0, 0)';
 
+const createDataset = ({color, ...props}) => ({
+  ...props,
+  borderColor: color,
+  backgroundColor: color,
+  pointRadius: 0.1,
+  cubicInterpolationMode: 'monotone',
+  fill: false,
+});
+
 const ctx = canvas.getContext('2d');
 const chart = new Chart(ctx, {
   type: 'line',
   data: {
     datasets: [
-      {
-        borderColor: '#FF0000',
-        backgroundColor: '#FF0000',
-        fill: false,
+      createDataset({
+        color: '#FF0000',
         label: 'Netto bestedingsvermogen',
         data: nettoData,
-      },
-      {
-        borderColor: '#00FF00',
-        backgroundColor: '#00FF00',
-        fill: false,
+      }),
+      createDataset({
+        color: '#00FF00',
         label: 'Huurtoeslag',
         data: generateSeries(hi => computeHuurtoeslag(hi, 700)),
-      },
-      {
-        borderColor: '#0000FF',
-        backgroundColor: '#0000FF',
-        fill: false,
+      }),
+      createDataset({
+        color: '#0000FF',
         label: 'Zorgtoeslag',
-        data: generateSeries(hi => computeZorgtoeslag(hi)),
-      },
-      {
-        borderColor: '#555555',
-        backgroundColor: '#555555',
-        fill: false,
+        data: generateSeries(computeZorgtoeslag),
+      }),
+      createDataset({
+        color: '#FFFF00',
+        label: 'Inkomstenbelasting',
+        data: generateSeries(computeInkomstenbelasting),
+      }),
+      createDataset({
+        color: '#FF00FF',
+        label: 'Heffingskorting',
+        data: generateSeries(computeHeffingskorting),
+      }),
+      createDataset({
+        color: '#00FFFF',
+        label: 'Arbeidskorting',
+        data: generateSeries(computeArbeidskorting),
+      }),
+      createDataset({
+        color: '#555555',
         label: 'Heffingsloon',
         data: generateSeries(hi => hi),
-      },
+      }),
     ]
   },
   options: {
